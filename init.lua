710be6ce086fc4c80f726a1e68dccd37107dd25e
Carioca <carioca@NaOca.NovaGuarida>
2017-09-21 17:08:28 -0300

    state fee or not dig/place

--- init.lua
+++ init.lua
@@ -10,14 +10,26 @@ local fee=function(player)
   if not minetest.check_player_privs(pname,"freedman") then
     if economy.withdraw(pname, FEE, "Interaction fee") then
       dogecoin.transfer(pname, SOURCE_DOGEACC, FEE, "Interaction fee")
+      return true
     end
+    return false
   end
+  return true
 end
 
 minetest.register_on_dignode(function(pos, oldnode, digger)
-  fee(digger)
+  if not fee(digger) then
+    --revert dignode
+    local inv = digger:get_inventory()
+    local drop = minetest.get_node_drops(oldnode, nil)[1]
+    inv:remove_item("main", drop)
+    minetest.set_node(pos, oldnode)
+  end
 end)
 
 minetest.register_on_placenode(function(pos, newnode, placer, oldnode, itemstack, pointed_thing)
-  fee(placer)
+  if not fee(placer) then
+    minetest.set_node(pos, oldnode)
+    return true
+  end
 end)
