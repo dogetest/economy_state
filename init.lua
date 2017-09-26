491de59a6d4469028c2dd6ca6f82e9dd8228fbe6
Carioca <carioca@NaOca.NovaGuarida>
2017-09-26 14:33:37 -0300

    wraps nodes on_place and on_dig instead register_on_place/dig

--- init.lua
+++ init.lua
@@ -1,15 +1,14 @@
---dogeconomy protection mod
-local FEE = 1
+--dogestate protection mod
+FEE = 1
 
 minetest.register_privilege("freedman", {
 	description = "Wont pay interaction fee.",
 })
 
-local fee=function(player)
-  local pname = economy.pname(player)
-  if not minetest.check_player_privs(pname,"freedman") then
-    if economy.withdraw(pname, FEE, "Interaction fee") then
-      dogecoin.transfer(pname, SOURCE_DOGEACC, FEE, "Interaction fee")
+local fee=function(nick)
+  if not minetest.check_player_privs(nick, "freedman") then
+    if economy.withdraw(nick, FEE, "Interaction fee") then
+      dogecoin.transfer(nick, SOURCE_DOGEACC, FEE, "Interaction fee")
       return true
     end
     return false
@@ -17,19 +16,70 @@ local fee=function(player)
   return true
 end
 
-minetest.register_on_dignode(function(pos, oldnode, digger)
-  if not fee(digger) then
-    --revert dignode
-    local inv = digger:get_inventory()
-    local drop = minetest.get_node_drops(oldnode, nil)[1]
-    inv:remove_item("main", drop)
-    minetest.set_node(pos, oldnode)
+--State wraps node's on_place and on_dig
+local wrap_on_place=function(on_place)
+  return function(itemstack, placer, pointed_thing)
+    local fee_and_place=function(itemstack, placer, pointed_thing)
+      local pname=economy.pname(placer)
+      if fee(pname) then
+        return on_place(itemstack, placer, pointed_thing)
+      else
+        return itemstack
+      end
+    end
+    if (minetest.registered_nodes[minetest.get_node(pointed_thing.under).name] or {}).on_rightclick then
+      if placer:get_player_control()['sneak'] then
+        return fee_and_place(itemstack, placer, pointed_thing)
+      else
+        return on_place(itemstack, placer, pointed_thing)
+      end
+    else
+      return fee_and_place(itemstack, placer, pointed_thing)
+    end
+    return itemstack
   end
-end)
+end
 
-minetest.register_on_placenode(function(pos, newnode, placer, oldnode, itemstack, pointed_thing)
-  if not fee(placer) then
-    minetest.set_node(pos, oldnode)
-    return true
+local wrap_on_dig=function(on_dig)
+  return function(pos, oldnode, digger)
+    local pname=economy.pname(digger)
+    if fee(pname) then
+      return on_dig(pos, oldnode, digger)
+    else
+      return true
+    end
   end
-end)
+end
+
+--Wrap registered nodes definitions
+local wrap_place_definition=function(name)
+  local old_def = minetest.registered_items[name]
+  local old_on_place = old_def.on_place or minetest.item_place
+  minetest.override_item(name, { on_place = wrap_on_place(old_on_place) })
+end
+
+local wrap_dig_definition=function(name)
+  local old_def = minetest.registered_items[name]
+  local old_on_dig = old_def.on_dig or minetest.node_dig
+  minetest.override_item(name, { on_dig = wrap_on_dig(old_on_dig) })
+end
+
+for item in ipairs(minetest.registered_items) do
+  wrap_place_definition(item.name)
+  wrap_dig_definition(item.name)
+end
+
+--Wrap minetest.register_node
+old_register_node=minetest.register_node
+
+local wrap_register_node=function(register_node)
+  return function(name, definition)
+    local old_on_place=definition.on_place or minetest.item_place
+    local old_on_dig=definition.on_dig or minetest.node_dig
+    definition.on_place=wrap_on_place(old_on_place)
+    definition.on_dig=wrap_on_dig(old_on_dig)
+    return register_node(name, definition)  
+  end
+end
+
+minetest.register_node=wrap_register_node(old_register_node)
